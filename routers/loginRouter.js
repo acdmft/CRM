db9c9e45644525d24c941da7650c7eda89c61a84
fusbudget <andrewsanser@gmail.com>
2022-03-30 15:22:30 +0200

    add router.post in loginRouter.js

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
+// USER MODEL 
+const User = require("../models/userModel");
 
 router.get("/", (req, res) => {
   res.json({message: "login router"});
+});
+
+router.post("/", async (req, res) => {
+  let user;
+  try {
+    user = await User.findOne({ $and: [{ "email": req.body.email }, { "password": req.body.pwd }] }).populate("contacts"); 
+  } catch(err) {
+    return res.json({message: err});
+  }
+  res.json(user);
 });
 
 module.exports = router;
